c13972a23cc87bb16d6ebc198833cb975e1b8dfd
Itachixtanjiro <96478749+Itachixtanjiro@users.noreply.github.com>
2022-01-19 00:43:26 +0530

    Add files via upload

---
+++ assignment 2 output.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:\Users\ASUS\Assignment 2.py ====================
+given input = Python is a case sensitive language
+string length = 35
+string reversed = egaugnal evitisnes esac a si nohtyP
+sting sliced = a case sensitive
+string replaced = Python is object oriented language
+index value of 'a' = 10
+whitespaces removed = Pythonisacasesensitivelanguage
+ Hey,  Priyanshu Here! 
+ My SID 21105060 
+ I am from Electronics and Communication Engineering department and my CGPA is 9.9
+value of 'A' = 56
+value of 'B' = 10
+A&B = 8
+A|B = 58
+A^B = 50
+left shift of 'A' by 2 bits = 224
+left shift of 'B' by 2 bits = 40
+right shift of 'A' by 2 bits = 14
+right shift of 'B' by 4 bits = 0
+Enter 1st number:67
+Enter 2nd number:87
+Enter 3rd number:98
+The largest number is 98.0
+given word = name
+string taken = my name is Tanjiro Kamado, what is your name ?
+yes, chosen word is preset.
+Enter 1st length = 25
+Enter 2nd length = 24
+Enter 3rd length = 7
+yes triangle is possible
